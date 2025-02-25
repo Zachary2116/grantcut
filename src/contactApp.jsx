2af341d34ef53d7d5ebd0c4879c5284eaf3a5506
Zachary Zhao <zziswang2@gmail.com>
2025-02-25 11:10:59 -0500

    contact_form_dev_commit

--- src/contactApp.jsx
+++ src/contactApp.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-
+import Swal from 'sweetalert2'
 import './contactApp.css';
 import './index.css';
 
@@ -24,19 +24,53 @@ function App() {
     
         return () => observer.disconnect(); // Cleanup observer on unmount
       }, []);
-       
-  return (
-    <div className="contact-page contact-header fade-in-section">
+      const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+    
+        formData.append("access_key", "65841ce8-cafc-4c2d-b1e3-b18c1dc403c4");
+    
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+    
+        const res = await fetch("https://api.web3forms.com/submit", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: json
+        }).then((res) => res.json());
+    
+        if (res.success) {
+          Swal.fire({
+            title: "Sent",
+            text: "Message sent Successfully!",
+            icon: "success"
+          });
+        }
+      };
+    return (
+      <section className="contact fade-in-section" >
+        <form onSubmit={onSubmit}>
+          <h2>Contact Form</h2>
+          <div className="input-box">
+            <label>Full Name</label>
+            <input type="text" className="field" placeholder="Enter your name" name = 'name' required />
+          </div>
+          <div className="input-box">
+            <label>Email Address</label>
+            <input type="email" className="field" placeholder="Enter your email" name = 'email' required />
+          </div>
+          <div className="input-box">
+            <label>Your Message</label>
+            <textarea name = 'message' className="field mess" placeholder="Enter your message" required></textarea>
+          </div>
+          <button type="submit-button">Send Message</button>
+        </form>
+      </section>
+    );
 
-      <h1>Contact Us</h1>
-      <p>
-        We would love to hear from you! Please contact us at
-        grantcutmarketing@gmail.com
-
-      </p>
-
-  </div>
-);
 }
 
 export default App;
